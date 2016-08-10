4c71a1a5a6cebe8f9bd0d005e7c6d884beeef4d3
eco-terrorist <distasteful.static@gmail.com>
2016-08-10 01:58:23 -0400

    Update testpw.py

--- testpw.py
+++ testpw.py
@@ -1,3 +1,9 @@
+#this script is useful for testing a limited amount of passwords without having to fill out captcha 
+#or locking the account. It also avoids, as far as I can see, triggering the "blocked sign in attempt" 
+#email. 
+#You will still get blocked after a certain number of attempts, so it's not suited for brute-forcing
+#or dictionary attacks
+
 import smtplib
 import getpass
 from email.mime.text import MIMEText
